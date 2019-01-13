1e898802c7bc5fc1177a8a346e22b8cd8a970dbe
Parv Jain <parvjn616@gmail.com>
2019-01-13 14:25:04 +0530

    CORS middleware added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -63,6 +63,10 @@ $app->singleton(
 //    App\Http\Middleware\ExampleMiddleware::class
 // ]);
 
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+]);
+
 $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
 ]);
